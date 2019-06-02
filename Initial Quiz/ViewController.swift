f89d221167e87aa441c0edd53f46829aad74fd94
Denis Bystruev <dbystruev@me.com>
2019-06-02 10:55:24 +0300

    Initial Commit

---
+++ Initial Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Initial Quiz
+//
+//  Created by Denis Bystruev on 02/06/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
