fa56ebc6501b995570193bc0c55220cc0599a6b7
Denis Bystruev <dbystruev@me.com>
2019-06-02 12:20:57 +0300

    Simplified App Delegate

---
+++ Initial Quiz/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Initial Quiz
+//
+//  Created by Denis Bystruev on 02/06/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
